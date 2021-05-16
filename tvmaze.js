289a59c6c82087a1a71333d6de13e9087d27323b
Taek Been Nam <taekbeen93@gmail.com>
2021-05-15 21:13:06 -0400

    add episodes functionalities

--- tvmaze.js
+++ tvmaze.js
@@ -1,28 +1,13 @@
-/** Given a query string, return array of matching shows:
- *     { id, name, summary, episodesUrl }
- */
+const searchForm = document.querySelector('#search-form');
+const episodes = document.querySelector('#episodes-list');
+const showsList = document.querySelector('#shows-list');
 
 
-/** Search Shows
- *    - given a search term, search for tv shows that
- *      match that query.  The function is async show it
- *       will be returning a promise.
- *
- *   - Returns an array of objects. Each object should include
- *     following show information:
- *    {
-        id: <show id>,
-        name: <show name>,
-        summary: <show summary>,
-        image: <an image from the show data, or a default imege if no image exists, (image isn't needed until later)>
-      }
- */
 async function searchShows(query) {
   // TODO: Make an ajax request to the searchShows api.  Remove
   const results = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`)
 
   const showList = results.data.map(showObj => {
-    console.log(showObj)
     return {
       id: showObj.show.id,
       name: showObj.show.name,
@@ -40,24 +25,25 @@ async function searchShows(query) {
  *     - given list of shows, add shows to DOM
  */
 
-function populateShows(shows) {
-  const $showsList = $("#shows-list");
-  $showsList.empty();
+const populateShows = (shows) => {
+  // showsList.empty();
 
   for (let show of shows) {
-    let $item = $(
-      `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
+    const item = document.createElement('div');
+    item.setAttribute('class', 'col-md-6 col-lg-3 Show')
+    item.innerHTML = 
+      `<div class="Show" data-show-id="${show.id}">
          <div class="card" data-show-id="${show.id}">
            <img class="card-img-top" src="${show.image}">
            <div class="card-body">
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
+             <button class="btn btn-primary episodes" type="button">Episodes</button>
            </div>
          </div>
        </div>
-      `);
-
-    $showsList.append($item);
+      `;
+    showsList.append(item);
   }
 }
 
@@ -67,17 +53,27 @@ function populateShows(shows) {
  *    - get list of matching shows and show in shows list
  */
 
-$("#search-form").on("submit", async function handleSearch (evt) {
+searchForm.addEventListener("submit", async function handleSearch (evt) {
   evt.preventDefault();
-
-  let query = $("#search-query").val();
+  console.log(evt)
+  const input = document.querySelector('#search-query');
+  let query = input.value.trim();
   if (!query) return;
 
-  $("#episodes-area");
+  // $("#episodes-area").show();
 
   let shows = await searchShows(query);
   populateShows(shows);
+  input.value = '';
 });
+
+// listen for episode button click inside each card.
+showsList.addEventListener("click",  evt => {
+  if(evt.target.type === 'button') {
+    
+  }
+});
+
 
 
 /** Given a show ID, return list of episodes:
@@ -98,5 +94,8 @@ async function getEpisodes(id) {
 }
 
 const populateEpisodes = (episodes) => {
-
+  const episodesList = document.querySelector('#episodes-list');
+  const episode = document.createElement('li');
+  episode.innerHTML = `${episodes.name} (season ${episodes.season}, number ${episodes.number})`
+  episodesList.append(episode);
 }
